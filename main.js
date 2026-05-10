842de0917fc8ae3113fcb67b728bb1b9758df01f
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2026-05-10 16:29:35 +0200

    feat: improve mobile menu keyboard accessibility
    - Implemented open/close toggle functionality
    - Added Escape key listener to close menu
    - Built a keyboard focus trap to prevent tabbing outside the open menu
    - Managed focus state so it returns to the open button when closed

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const menuOpenBtn = document.querySelector(".menu-open-btn");
 const menuCloseBtn = document.querySelector(".menu-close-btn");
+const navigation = document.querySelector(".primary-navigation");
 const navList = document.querySelector(".nav-list");
 const overlay = document.querySelector(".overlay");
+const focusableElements = navigation.querySelectorAll(
+  `button:not(.menu-open-btn), .nav-link`,
+);
+const firstFocusableElement = focusableElements[0];
+const lastFocusableElement = focusableElements[focusableElements.length - 1];
 
 menuOpenBtn.addEventListener("click", openMenu);
 
@@ -9,6 +15,15 @@ menuCloseBtn.addEventListener("click", closeMenu);
 
 document.addEventListener("keydown", (e) => {
   if (navList.classList.contains("show") && e.key === "Escape") closeMenu();
+  if (navList.classList.contains("show") && e.key === "Tab") {
+    if (e.shiftKey && document.activeElement === firstFocusableElement) {
+      e.preventDefault();
+      lastFocusableElement.focus();
+    } else if (!e.shiftKey && document.activeElement === lastFocusableElement) {
+      e.preventDefault();
+      firstFocusableElement.focus();
+    }
+  }
 });
 
 overlay.addEventListener("click", closeMenu);
